f50d74fb54fa45899b99c40918ae30f98d7046aa
Trần Cảnh Dinh <40481356+dinhdeveloper@users.noreply.github.com>
2018-10-29 07:05:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dinht
- * Date: 10/23/2018
- * Time: 11:03 AM
- */
 include("Lib/DB.php");
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
@@ -22,33 +16,34 @@ if (isset($_GET["id"])) {
     <link rel="icon" href="Lib/backgroud.jpg">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!--     <link rel="stylesheet" href="https://code.jquery.com/jquery-3.3.1.min.js">
-    <link rel="stylesheet" href="jquery/jquery-3.3.1.js">
-    <link rel="stylesheet" href="jquery/jquery-3.3.1.min.js">
-
+    <script type="text/javascript" src="jquery/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="jquery/jquery-3.3.1.js"></script>
     <script>
-        $(document).ready(function(){
-            $("#bookname").blur(function () {
-                var bookname = $("#bookname").val();
+        $(document).ready(function () {
+            $("#id").blur(function () {
+                var id = $(this).val();
                 $.ajax({
-                    url:"KiemTra.php",
-                    type: "get",
+                    url: "KiemTra.php",
+                    type: "post",
                     dataType: "text",
-                    data:{
-                        un:bookname,
+                    data: {
+                        un: id,
                     },
-                    success:function (data) {
+                    success: function (data) {
                         if (data == 1) {
-                            $("#kiemtra").css("color", "red").html("Mã đã tồn tại");
+                            $("#kiemtra").css("color", "red").html("Mã đã tồn tại.");
+                            // $.button("#submit").type = "hidden";
+                            $("#bookname").css("type", "hidden");
+
                         }
                         else {
-                            $("#kiemtra").css("color", "blue").html("Hợp lệ");
+                            $("#kiemtra").css("color", "blue").html("Mã hợp lệ.");
                         }
                     }
                 })
-            })
+            });
         });
-    </script> -->
+    </script>
 </head>
 <body>
 <div class="container">
@@ -69,14 +64,14 @@ if (isset($_GET["id"])) {
                 </thead>
                 <tbody>
                 <?php
-                $sql = "SELECT * FROM `book` WHERE (`add` = 1 OR `add` = 0)";
+                $sql = "SELECT * FROM `book`";
                 $result = Database::ExecuteQuery($sql);
                 while ($row = mysqli_fetch_array($result)) {
                     ?>
                     <tr>
-                        <th scope="row"><?php echo $row['id']?></th>
-                        <td><?php echo $row['bookname']?></td>
-                        <td><a href="CapNhat.php?id=<?php echo $row['id']?>"><img src="Lib/delete.png"></a></td>
+                        <th scope="row"><?php echo $row['id'] ?></th>
+                        <td><?php echo $row['bookname'] ?></td>
+                        <td><a href="CapNhat.php?id=<?php echo $row['id'] ?>"><img src="Lib/delete.png"></a></td>
                     </tr>
                     <?php
                 }
@@ -100,8 +95,8 @@ if (isset($_GET["id"])) {
                 while ($row = mysqli_fetch_array($result)) {
                     ?>
                     <tr>
-                        <th scope="row"><?php echo $row['id']?></th>
-                        <td><?php echo $row['bookname']?></td>
+                        <th scope="row"><?php echo $row['id'] ?></th>
+                        <td><?php echo $row['bookname'] ?></td>
                     </tr>
                     <?php
                 }
@@ -111,31 +106,32 @@ if (isset($_GET["id"])) {
         </div>
         <div class="col-md-3">
             <h4 style="color: blue"><u>THÊM SÁCH</u></h4><br>
-            <form class="form-inline" method="get">
+            <form class="form-inline" method="post">
                 <div class="form-group mx-sm-3 mb-2">
                     <label for="bookname" class="sr-only">ID</label>
                     <input type="text" class="form-control" id="id" placeholder="ID" name="id">
                 </div>
-
+                <div id="kiemtra"></div>
+                <br>
+                <br>
                 <div class="form-group mx-sm-3 mb-2">
                     <label for="bookname" class="sr-only">Book Name</label>
                     <input type="text" class="form-control" id="bookname" placeholder="Tên Sách" name="bookname">
                 </div>
-                <div id="kiemtra"></div>
-                <button type="submit" class="btn btn-primary mb-2" name="submit" style="margin-left:20px">Gửi</button>
+                <button type="submit" id="submit" class="btn btn-primary mb-2" name="submit" style="margin-left:20px">
+                    Gửi
+                </button>
             </form>
             <?php
             if (isset($_REQUEST['submit'])) {
-                $id = $_GET['id'];
-                $bookname = $_GET['bookname'];
-//                $add = $_POST['add'];
-                if ($id == "") {
+                $id = $_POST['id'];
+                $bookname = $_POST['bookname'];
+                if ($id == "" || $bookname == "") {
                     echo '<script>alert("Vui lòng nhập đầy đủ trường thông tin")</script>';
                 } else {
                     $sql = "INSERT INTO book(id,bookname)
-					VALUES ('$id','$bookname')";
+					            VALUES ('$id','$bookname')";
                     $result = Database::ExecuteQuery($sql);
-                    echo '<script>alert("Thêm thành công")</script>';
                     Database::ChangeURL("index.php");
                 }
             }
